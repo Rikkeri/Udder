851e4e77a80c6a7b19b2ca8e30f3ef96fccfe0af
Riku Karppinen <rikk@uef.fi>
2019-07-02 07:15:45 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Riku Karppinen on 02/07/2019.
+//  Copyright © 2019 Riku Karppinen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
